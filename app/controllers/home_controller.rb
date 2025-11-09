9d44840b1234a1656175659feace236e545f7870
rvillasevil <rvillasevil@gmail.com>
2025-11-09 09:57:59 +0100

    HOMASON MVP API homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,7 @@
-class HomeController < ApplicationController
-  skip_before_action :set_current_user
+class HomeController < ActionController::Base
+  layout false
 
   def index
-    # The homepage is a static marketing view rendered from
-    # app/views/home/index.html.erb. We don't need to perform
-    # any controller work other than skipping authentication.
+    render template: "index"
   end
 end
