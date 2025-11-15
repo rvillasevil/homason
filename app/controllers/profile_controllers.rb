f79e0727fb26c6ee0c6bfd88181ed3b308ea8e77
rvillasevil <rvillasevil@gmail.com>
2025-11-15 13:42:14 +0100

    error rtf token

--- app/controllers/profile_controllers.rb
+++ app/controllers/profile_controllers.rb
@@ -1,4 +1,4 @@
-class ProfilesController < ApplicationController
+class ProfileController < ApplicationController
   before_action :require_login
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root "home#index"
 
   resource :session, only: %i[new create destroy]
-  resource :profile, only: %i[show update], controller: :profiles
+  resource :profile, only: %i[show update]
   resource :dashboard, only: :show, controller: :dashboards, as: :dashboard
   resources :users, only: %i[new create]
 end
